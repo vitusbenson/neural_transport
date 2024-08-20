b6659a3db6865800eb3cf5706ed9d66c4f6ef548
vitusbenson <vitus.benson@icloud.com>
2024-08-20 09:09:37 +0200

    keep only on sfno

--- neural_transport/models/__init__.py
+++ neural_transport/models/__init__.py
@@ -2,7 +2,8 @@ from neural_transport.models.gnn.gnn import GraphTM
 from neural_transport.models.gnn.graphcast import GraphCast
 from neural_transport.models.hybridsfno import HybridSFNO
 from neural_transport.models.sfno import SFNO
-from neural_transport.models.sfno_v2 import SFNOv2
+
+# from neural_transport.models.sfno_v2 import SFNOv2
 from neural_transport.models.swintransformer import SwinTransformer
 from neural_transport.models.unet import UNet
 
