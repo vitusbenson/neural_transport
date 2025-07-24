984eb7aea45bf446c5a899628903176653118809
grossjonathan <jonathan.gross@stud.uni-goettingen.de>
2025-07-24 11:38:54 +0200

    Wrappers registry to avoid circularity in MODEL init

---
+++ neural_transport/models/wrappers_registry.py
@@ -0,0 +1,7 @@
+from neural_transport.models.flowmatching import FlowMatching
+# from neural_transport.models.flowmatching import VelocityWrapper
+
+MODELWRAPPERS = {
+    "flowmatching": FlowMatching,
+    # "velocitywrapper": VelocityWrapper,
+}
