c8e7a3ddf6e2670776bedcb2b76442df588e9646
grossjonathan <jonathan.gross@stud.uni-goettingen.de>
2025-07-29 15:15:46 +0200

    change from dictionary to torch.tensor integrated into library

--- neural_transport/models/flowmatching.py
+++ neural_transport/models/flowmatching.py
@@ -1,9 +1,8 @@
-from typing import Dict, Optional
+from typing import Optional
 
 # torch
 import torch
 import torch.nn as nn
-
 
 # flow_matching
 from flow_matching.path.scheduler import CondOTScheduler
@@ -12,98 +11,79 @@ from flow_matching.solver import ODESolver
 
 # neural_transport
 from neural_transport.models import MODELS
+from neural_transport.models.regulargrid import RegularGridModel
 # from neural_transport.models.unet import UNet
 
 class VelocityWrapper(nn.Module):
     def __init__(
         self,
-        model: nn.Module,
-        target_vars: str,
-        offset: Optional[torch.Tensor] = None,
-        scale: Optional[torch.Tensor] = None,
-        delta_offset: Optional[torch.Tensor] = None,
-        delta_scale: Optional[torch.Tensor] = None,
-        static_batch: Optional[Dict[str, torch.Tensor]] = None,
+        submodel: nn.Module,
+        nlev: int = 1,
+        static_inputs: Optional[torch.Tensor] = None,
     ):
         super().__init__()
-        self.model = model
-        self.target_vars = target_vars
-
-        self.offset = offset
-        self.scale = scale
-        self.delta_offset = delta_offset
-        self.delta_scale = delta_scale
-
-        self.static_batch = static_batch or {}
+        self.submodel = submodel
+        self.nlev = nlev
+        self.static_inputs = static_inputs if static_inputs is not None else torch.empty(0)
 
     def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
-        batch_vf = {
-            self.target_vars: x,
-            "flow_time": t,
-            f"{self.target_vars}_offset": self.offset,
-            f"{self.target_vars}_scale": self.scale,
-            f"{self.target_vars}_delta_offset": self.delta_offset,
-            f"{self.target_vars}_delta_scale": self.delta_scale,
-        }
-
-        batch_vf.update(self.static_batch)
-        out = self.model(batch_vf)
-        return out[self.target_vars]
+        B, _, Nlat, Nlon = x.shape
+        t_expanded = t.view(1, 1, 1, 1).expand(B, 1, Nlat, Nlon)
+        x_in = torch.cat(
+            [x, t_expanded] + [self.static_inputs], dim=1
+        ) # [B C_total Nlat Nlon]
+        out = self.submodel.model(x_in)
+        return out[:, :self.nlev, :, :]
     
 
-class FlowMatching(nn.Module):
-    def __init__(
+class FlowMatching(RegularGridModel):
+    def init_model(
             self,
-            model="unet",
+            submodel="unet",
             model_kwargs={},
-            input_vars=[],
             return_intermediates=False,
             method='midpoint',
             nlev=1,
-            step_size=0.01
+            step_size=0.01,
             ):
-        super().__init__()
-
-        required_input_vars = ["x_t", "flow_time"]
-        input_vars = model_kwargs.get("input_vars", [])
-        for var in reversed(required_input_vars):
-            if var not in input_vars:
-                input_vars.insert(0, var)
-
-        model_kwargs = model_kwargs.copy()
-        model_kwargs["input_vars"] = input_vars
-        model_kwargs["model_kwargs"]["in_chans"] += 1 + nlev * 1 # + 1 for flow_time, + nlev for co2massmix
-
-        self.model = MODELS[model](**model_kwargs) # UNet(**sub_kwargs)
+        
+        self.submodel = MODELS[submodel](**model_kwargs)
         self.return_intermediates = return_intermediates
         self.method = method
+        self.nlev = nlev
         self.step_size = step_size
         self.path = AffineProbPath(scheduler=CondOTScheduler())
-        self.target_vars = self.model.target_vars[0] # Here target_vars[0] is supposed to be "co2massmix"
-
+        self.target_vars = self.submodel.target_vars # Here target_vars[0] is supposed to be "co2massmix"
+        # self.velocity_model = VelocityWrapper(self.submodel, self.target_vars)
+    
     def forward(self, batch):
         if self.training:
             return self.training_forward(batch)
+        elif self.return_intermediates:
+            x_in = self.preprocess_inputs(batch)
+            trajectory = self.model(x_in)
+            x_out = trajectory[-1]
+            sol = self.postprocess_outputs(x_out, batch)
+            return sol, trajectory
         else:
-            return self.inference_forward(batch)
+            return super().forward(batch)
 
-
+    # training
     def training_forward(self, batch):
-        # extract target_vars to get x_1 and normalize, [B N C]
-        x_1 = batch[f"{self.target_vars}_next"]
-        batch_normalized = self.model.normalize_batch_target_vars(batch)
-        x_1_normalized = batch_normalized[f"{self.target_vars}_next"]
+        # sample data [B C Nlat Nlon]
+        x_in = self.preprocess_inputs(batch)
+        # extract target_vars to get x_1 [B C Nlat Nlon] and normalize
+        batch_normalized = self.normalize_batch_target_vars(batch)
+        x_1_normalized = batch_normalized[f"{self.target_vars[0]}_next"]
+        B, _, C = x_1_normalized.shape
+        x_1_normalized = x_1_normalized.reshape(B, self.nlat, self.nlon, C).permute(0, 3, 1, 2)
 
-        # sample noise  x_0 ~ N(0, I), [B N C]
-        x_0 = torch.randn_like(x_1, device=x_1.device)
+        # sample noise  x_0 ~ N(0, I), [B C Nlat Nlon]
+        x_0 = torch.randn_like(x_in, device=x_in.device)
 
-        # sample time t \in [0,1], [B] -> [B N 1]
-        B, N, _ = x_1.shape
-        t = torch.rand(B, device=x_1.device)
-        t_expanded = t[:, None, None].expand(B, N, 1)  # expand [B N 1] to match x_1 shape
-        batch["flow_time"] = t_expanded
-        batch["flow_time_offset"] = torch.zeros_like(t_expanded)
-        batch["flow_time_scale"] = torch.ones_like(t_expanded)
+        # sample time t \in [0,1], [B] -> [B 1 Nlat Nlon]
+        B, _, nlat, nlon = x_in.shape
+        t = torch.rand(B, device=x_in.device)
 
         # sample path
         path_sample = self.path.sample(
@@ -112,37 +92,37 @@ class FlowMatching(nn.Module):
             x_1=x_1_normalized
         )
 
-        # compute flow matching loss and add denormalized x_1
+        # sample x_t from the path
         # access scheduler for affine path
         scheduler_out = self.path.scheduler(t)
-        d_sigma_t = scheduler_out.d_sigma_t.view(-1, 1, 1)
-        d_alpha_t = scheduler_out.d_alpha_t.view(-1, 1, 1)
+        d_sigma_t = scheduler_out.d_sigma_t.view(-1, 1, 1, 1)
+        d_alpha_t = scheduler_out.d_alpha_t.view(-1, 1, 1, 1)
 
-        batch["x_t"] = (path_sample.x_t - d_sigma_t * x_0) / d_alpha_t
-        # path_sample.x_t + x_0 (simplified version)
-        batch["x_t_offset"] = torch.zeros_like(batch["x_t"])
-        batch["x_t_scale"] = batch["x_t"].std(dim=(1, 2), keepdim=True)
-        ### why dim=(1, 2) everywhere? why not over the entire batch?
-        preds = self.model(batch)
+        x_t = (path_sample.x_t - d_sigma_t * x_0) / d_alpha_t
+        # x_t = path_sample.x_t + x_0 (simplified version)
+        # this requires target_vars to be first in input_vars and all nlev-dimensional
+        x_in[:, :self.nlev*len(self.target_vars), :, :] = x_t  # [B C Nlat Nlon] 
 
-        return preds # [B N C]
+        t_expanded = path_sample.t.view(-1, 1, 1, 1).expand(B, 1, nlat, nlon)
+        x_in = torch.cat([x_in, t_expanded], dim=1)  # [B C+1 Nlat Nlon]
 
-    def return_velocity_wrapper(self, offset, scale, delta_offset, delta_scale, batch):
+        x_out = self.submodel.model(x_in)
+
+        return x_out #, x_1_normalized
+
+    def return_velocity_wrapper(self, submodel, static_inputs):
         return VelocityWrapper(
-            self.model,
-            self.target_vars,
-            offset=offset,
-            scale=scale,
-            delta_offset=delta_offset,
-            delta_scale=delta_scale,
-            static_batch=batch
+            submodel=submodel,
+            nlev=self.nlev,
+            static_inputs=static_inputs,
         )
 
-    def inference_forward(self, batch):
-        # sample noise to get x0 [B N C]
-        all_levels = batch[self.target_vars] # [B N C]
-        x_init = torch.randn(*all_levels.shape, device=batch[self.target_vars].device)
-        #surface_level = batch[self.target_vars][:,:,0:1] # [B N 1]
+    # inference_forward
+    def model(self, x_in):
+        # sample noise to get x_init [B C Nlat Nlon]
+        all_levels = x_in[:, :self.nlev*len(self.target_vars), :, :]  # [B C Nlat Nlon]
+        x_init = torch.randn(*all_levels.shape, device=x_in.device)
+        #surface_level = x_in[:, :1, :, :]  # [B 1 Nlat Nlon]
         #x_init = torch.randn(*surface_level.shape, device=batch[self.target_vars].device)
 
         # get timesteps for integration [T]
@@ -150,22 +130,18 @@ class FlowMatching(nn.Module):
 
         # UNet expects normalization parameters
         velocity_model = self.return_velocity_wrapper(
-            batch[f"{self.target_vars}_offset"],
-            batch[f"{self.target_vars}_scale"],
-            batch[f"{self.target_vars}_delta_offset"],
-            batch[f"{self.target_vars}_delta_scale"],
-            batch
+            submodel=self.submodel,
+            static_inputs=x_in[:, self.nlev*len(self.target_vars):, :, :],  # [B C Nlat Nlon] (static inputs)
         )
 
         # solve the ODE to get the trajectory
         solver = ODESolver(velocity_model=velocity_model)
-        sol = solver.sample(time_grid=time_grid,
-                                 x_init=x_init, method=self.method,
-                                 step_size=self.step_size,
-                                 return_intermediates=self.return_intermediates
-                                )
-        # denormalize sol
-        return sol
+        trajectory = solver.sample(time_grid=time_grid,
+                            x_init=x_init, method=self.method,
+                            step_size=self.step_size,
+                            return_intermediates=self.return_intermediates
+        ) # [T B C Nlat Nlon]
+        return trajectory
     
     #def inference_obs_forward(self, batch):
     #    return sol
